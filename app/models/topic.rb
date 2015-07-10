53ad9b91314ce9eabdef5c1883ab45a91ae1b31b
Ben Muschol <benmuschol@gmail.com>
2015-07-10 12:34:17 -0400

    prepare to branch

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,5 @@
 class Topic < ActiveRecord::Base
   has_many :topicings
   has_many :cases, through: :topicings
+  validates :name, unqiueness: { case_sensitive: false }
 end
--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -1,6 +1,7 @@
 class Viewer < ActiveRecord::Base
   has_many :viewerings
   has_many :cases, through: :viewerings
+  validates :name, uniqueness: { case_sensitive: false }
 
   def ==(other_object)
     other_object.instance_of?(Viewer) &&
