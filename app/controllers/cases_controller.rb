89417cd8bb7b441fb2df15c448e85f9f8436f348
Ben Muschol <muschol.b@husky.neu.edu>
2016-05-09 17:33:13 -0400

    prevent double-renderring

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -28,8 +28,7 @@ class CasesController < ApplicationController
     if params[:visibility] != @case.visibility && current_user != @case.user
       flash[:danger] = "You cannot change the visibility of a case that isn't yours."
       redirect_to @case
-    end
-    if @case.update_attributes(case_params)
+    elsif @case.update_attributes(case_params)
       flash[:success] = "Case updated"
       redirect_to @case
     else
