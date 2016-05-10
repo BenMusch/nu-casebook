0aa8164ea236d04cf5c566dc241524f7b67a917b
Ben Muschol <muschol.b@husky.neu.edu>
2016-05-10 00:11:49 -0400

    emma

--- app/assets/not_seth_validator.rb
+++ app/assets/not_seth_validator.rb
@@ -2,7 +2,7 @@ class NotSethValidator < ActiveModel::EachValidator
   def validate_each(person, attribute, value)
     if value == 'goldstein.se@husky.neu.edu'
       person.errors[:base] << "Nice try, Seth."
-    elsif !(value =~ /\A(\w)+\.(\w)+@husky.neu.edu/i)
+    elsif !(value =~ /\A[A-Za-z\-]+\.\w+@husky.neu.edu/i)
       person.errors[attribute] << "is not a @husky.neu.edu address"
     end
   end
